5a179cd6c25095db7674dfcf02c63d5a60e375f1
Ed Charbeneau <ed.charbeneau@gmail.com>
2020-01-13 12:31:49 -0500

    Add project files.

---
+++ ComponentLibs/Components/Spinners/SpinnerType.cs
@@ -0,0 +1,17 @@
+﻿namespace ComponentLibs.Components.Spinners
+{
+    public enum SpinnerType
+    {
+        Bounce,
+        Chase,
+        Circle,
+        CircleFade,
+        Flow,
+        Grid,
+        Plane,
+        Pulse,
+        Swing,
+        Wander,
+        Wave
+    }
+}
